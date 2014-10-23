c5e750eb53fd96b67e935ac50afc56bf69209c8f
kelly <c_h_e_n_l_e_i@126.com>
2014-10-23 16:36:39 +0800

    add consumer

--- app/src/main/java/com/example/kelly/servicecontenttest/MyService.java
+++ app/src/main/java/com/example/kelly/servicecontenttest/MyService.java
@@ -2,15 +2,33 @@ package com.example.kelly.servicecontenttest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
-public class MyService extends Service {
-    public MyService() {
+public class MyService extends Service
+{
+    public MyService()
+    {
     }
 
+    public int getnum()
+    {
+        return 3;
+    }
     @Override
-    public IBinder onBind(Intent intent) {
+    public IBinder onBind(Intent intent)
+    {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.d("MyService", "return");
+        return new MyBind();
+    }
+
+    public class MyBind extends Binder
+    {
+        public MyService getMyService()
+        {
+            return MyService.this;
+        }
     }
 }
